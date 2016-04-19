be34430e49e694e3b1cd268584742155bdd380b0
Igor Andrade <andrade.igor@gmail.com>
2016-04-19 09:04:11 -0300

    busca html de adm

--- src/main/java/br/com/cerveja/controller/Index.java
+++ src/main/java/br/com/cerveja/controller/Index.java
@@ -1,8 +1,13 @@
 package br.com.cerveja.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerMapping;
+import org.thymeleaf.util.StringUtils;
 
 @Controller
 public class Index {
@@ -26,5 +31,12 @@ public class Index {
 	    @RequestMapping("/logout")
 	    public String logout() {
 		return "login";
+	    }
+	    @GET
+	    @RequestMapping("/adm/**")
+	    public String home(HttpServletRequest request) {
+	    	String restOfTheUrl = (String) request.getAttribute(
+	    	        HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+	    	return StringUtils.substringBefore(restOfTheUrl, ".html");
 	    }
 }

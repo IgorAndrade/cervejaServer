9f1ce4e412b1d60bef5471b822f1b6d773063ab9
Igor Andrade <andrade.igor@gmail.com>
2016-04-28 13:37:34 -0300

    atualizando controlador

--- src/main/java/br/com/cerveja/controller/CervejaCtl.java
+++ src/main/java/br/com/cerveja/controller/CervejaCtl.java
@@ -6,19 +6,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cerveja.model.cerveja.Cerveja;
+import br.com.cerveja.service.CervejaService;
 import br.com.cerveja.service.CloundCervejaService;
+import br.com.cerveja.service.impl.breweryDB.CloundCervejaServiceImpl;
 
 import com.sosv.breweryDB.connector.entity.beer.Beer;
 @RestController
 @RequestMapping(value = "/service/cerveja")
 public class CervejaCtl {
 	@Autowired
-	private CloundCervejaService importador;
+	private CervejaService service;
 	
 	@RequestMapping(value = "/importar/{id}")
-	public ResponseEntity<Beer> importar(@PathVariable("id")String id){
-		Beer beer = importador.importar(id);
+	public ResponseEntity importar(@PathVariable("id")String id){
+		Cerveja cerveja = service.importar(id);
 		
-		return ResponseEntity.ok(beer);
+		if(cerveja!=null)
+		return ResponseEntity.ok(cerveja);
+		else
+		return ResponseEntity.notFound().build();
 	}
 }
